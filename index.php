5561f9091e75d80044e164630890ee19d3b822e1
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-09-09 06:06:20 -0300

    Montagem da index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title>Curso Wordpress</title>
+</head>
+<body <?php body_class(); ?>>
+<p>Este é um parágrafo</p>
+<header>
+	<div class="barra-topo">
+		<div class="redes-sociais">
+			<div class="pesquisa">
+				
+
+			</div>
+		</div>
+
+		</div>
+	<div class="area-menu">
+		<div class="logo">
+			
+			<div class="menu">
+				
+
+
+			</div>
+		</div>
+
+	</div>
+</header>
+
+<div class="conteudo">
+	<main>
+		
+		<section class="slide"></section>
+		<section class="servicos"></section>
+		<section class="meio">
+			<aside class="barra-lateral"></aside>
+	
+			<div class="noticias"></div> 
+		</section>
+		<section class="mapa"></section>
+	</main>
+
+</div>
+
+<footer>
+	
+	
+</footer>
+
+</body>
+</html>
