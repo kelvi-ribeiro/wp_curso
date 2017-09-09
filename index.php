c2c2513c62204fc47d37f57a1bae33852ef6c96e
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-09-09 06:22:38 -0300

    Dividindo o conteúdo com Footer e Header

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-	<title>Curso Wordpress</title>
-</head>
-<body <?php body_class(); ?>>
-<p>Este é um parágrafo</p>
-<header>
-	<div class="barra-topo">
-		<div class="redes-sociais">
-			<div class="pesquisa">
-				
 
-			</div>
-		</div>
-
-		</div>
-	<div class="area-menu">
-		<div class="logo">
-			
-			<div class="menu">
-				
-
-
-			</div>
-		</div>
-
-	</div>
-</header>
-
+<?php get_header(); ?>
 <div class="conteudo">
 	<main>
 		
@@ -44,10 +15,5 @@
 
 </div>
 
-<footer>
-	
-	
-</footer>
+<?php get_footer(); ?>
 
-</body>
-</html>
